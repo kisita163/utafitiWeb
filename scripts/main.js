22c90dee7bf57ac4a1ae3fcfeebcd80e5854c15f
kisita <kisita2002@yahoo.fr>
2018-12-30 15:21:54 -0800

    Possibility to start new campaign

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBSu9flb1cs58EAbtCbWUKomVCfXkTKYeU",
+    apiKey: "AIzaSyBSu9flb1cs58EAbtCbWUKomVCfX}kTKYeU",
     authDomain: "caritas-50fab.firebaseapp.com",
     databaseURL: "https://caritas-50fab.firebaseio.com",
     projectId: "caritas-50fab",
@@ -15,8 +15,38 @@ var passw = env.PASS;
   
 firebase.initializeApp(config);   
 firebase.auth().signOut();
-firebase.auth().signInWithEmailAndPassword(email, passw).catch(function(error) {
+firebase.auth().signInWithEmailAndPassword(email,passw).catch(function(error) {
 	// Handle Errors here.
 	var errorCode = error.code;
 	var errorMessage = error.message;
 	  });
+
+document.getElementById('File').addEventListener('change', newCampaign, false);
+
+function newCampaign(evt){
+	var r = confirm("Êtes vous sûr de vouloir commencer une nouvelle campagne?");
+	
+	if (r == true) {
+		// Check for the various File API support.
+		if (window.File && window.FileReader && window.FileList && window.Blob) {
+	        // Delete current survey data. Save it somewhere maybe?
+	        var db = firebase.database().ref('survey').remove();
+	        
+			var files = evt.target.files; 
+			var file  = files[0];
+			// Create the file metadata
+			var metadata = {
+			  contentType: 'application/json'
+			};
+			// Create a root reference
+			var storageRef = firebase.storage().ref();
+			// Create a reference to 'images/mountains.jpg'
+			var fileToUpload = storageRef.child('current_survey/survey.json');
+			// Upload file and metadata to the object 'images/mountains.jpg'
+			var uploadTask = storageRef.child('current_survey/' + file.name).put(file, metadata);
+			
+		} else {
+		  alert('The File APIs are not fully supported in this browser.');
+		}
+	}
+}
